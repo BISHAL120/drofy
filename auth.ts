7113e1111693fa31f4b723bcfe448d65d5255559
MD BISHAL <monerulmd5@gmail.com>
2025-05-20 01:40:50 +0600

    prisma build command added

--- auth.ts
+++ auth.ts
@@ -58,14 +58,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     events: {
         async linkAccount({ user }) {
-            await db.user.update({
-                where: {
-                    id: user.id,
-                },
-                data: {
-                    emailVerified: new Date(),
-                },
-            });
+            console.log(user)
+            /*    await db.user.update({
+                   where: {
+                       id: user.id,
+                   },
+                   data: {
+                       emailVerified: new Date(),
+                   },
+               }); */
             /* if (user.id) {
               await db.session.create({
                 data: {
